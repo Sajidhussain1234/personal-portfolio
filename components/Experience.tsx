84913b0e0e5f0a871c6a5ad30cff8a68b600eaa5
Sajidhussain123 <sajidmcsbwn@gmail.com>
2024-02-07 18:53:03 +0500

    made some changes

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -5,7 +5,10 @@ import React from "react";
 const Experience = () => {
   return (
     <section id="experience">
-      <h1 className="text-white font-semibold text-center text-6xl pt-[35px] mt-[78px]">
+      <h1
+        className="text-white font-semibold text-center text-6xl pt-[35px] mt-[78px]"
+        style={{ transform: "scale(0.9" }}
+      >
         EXPERIENCE
       </h1>
       <p className=" tracking-[0.5em] text-center text-transparent font-light pb-5  bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500  text-1xl ">
