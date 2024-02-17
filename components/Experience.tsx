f91cc0386f9e09886eeb9f3e3a104b8e86aa9571
Sajid Hussain <sajidmcsbwn@gmail.com>
2024-02-17 11:39:59 +0500

    made some improvements

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -93,9 +93,6 @@ const Experience = () => {
             Node JS
           </div>
           <div className="bg-transparent  mt-5 mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            Node JS
-          </div>
-          <div className="bg-transparent  mt-5 mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
             Express JS
           </div>
           <div className="bg-transparent  mt-5 mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
